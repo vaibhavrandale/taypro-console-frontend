a05070e4671903e2f539c2a5eed9c4d521d34df9
vaibhavrandale <127591217+vaibhavrandale@users.noreply.github.com>
2025-02-20 09:57:54 +0530

    new build created

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -249,7 +249,6 @@ const Login = () => {
       dispatch({ type: 'EMP_SIGNIN', payload: data.data.user });
       localStorage.setItem('userInfo', JSON.stringify(data.data.user));
       localStorage.setItem('authtoken', JSON.stringify(data.data.token));
-
       navigate('/user-dashboard');
       // toast.success(`Login Successfull!`);
       toast.success(`Welcome Back!  ${data.data.user.username}`);
