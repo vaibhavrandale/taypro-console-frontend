ddc1548820c5f58962ad9aaed24a6f0696df4ab6
Sneha Araseed <sneha.a@taypro.in>
2025-03-04 11:56:41 +0530

    removed authtoken after logging out

--- src/store.js
+++ src/store.js
@@ -20,31 +20,32 @@
 
 // working
 
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore } from "redux";
 
 // Get userInfo from localStorage
 const initialState = {
   // sidebarShow: true,
-  theme: 'light',
-  userInfo: localStorage.getItem('userInfo')
-    ? JSON.parse(localStorage.getItem('userInfo'))
+  theme: "light",
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
-  authtoken: localStorage.getItem('authtoken')
-    ? JSON.parse(localStorage.getItem('authtoken'))
+  authtoken: localStorage.getItem("authtoken")
+    ? JSON.parse(localStorage.getItem("authtoken"))
     : null,
 };
 
 // Reducer function
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'EMP_SIGNIN':
+    case "EMP_SIGNIN":
       return { ...state, userInfo: action.payload };
 
-    case 'EMP_SIGNOUT':
-      localStorage.removeItem('userInfo'); // Remove from localStorage
+    case "EMP_SIGNOUT":
+      localStorage.removeItem("userInfo"); // Remove from localStorage
+      localStorage.removeItem("authtoken"); // Remove from localStorage
       return { ...state, userInfo: null };
 
-    case 'set':
+    case "set":
       return { ...state, ...action.payload };
 
     default:
