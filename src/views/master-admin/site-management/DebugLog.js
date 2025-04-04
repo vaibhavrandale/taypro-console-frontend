549dbe83839d2577f1f611d89c5e4259334cd48e
vaibhavrandale <127591217+vaibhavrandale@users.noreply.github.com>
2025-04-04 11:02:22 +0530

    extra pagination removed from debug log page and cleaning log page

--- src/views/master-admin/site-management/DebugLog.js
+++ src/views/master-admin/site-management/DebugLog.js
@@ -263,41 +263,6 @@ const DebugLog = () => {
           limit={limit}
           handleLimitChange={setLimit} // New prop
         />
-
-        <CRow className="mt-3">
-          <CCol className="d-flex justify-content-end">
-            <CButton
-              color="secondary"
-              disabled={!hasPrevPage}
-              onClick={() => handlePageChange(page - 1)}
-              className="mx-1"
-              size="sm"
-            >
-              Prev
-            </CButton>
-
-            {Array.from({ length: totalPages }, (_, i) => (
-              <CButton
-                key={i + 1}
-                color={page === i + 1 ? "primary" : ""}
-                onClick={() => handlePageChange(i + 1)}
-                className="mx-1"
-              >
-                {i + 1}
-              </CButton>
-            ))}
-
-            <CButton
-              color="secondary"
-              disabled={!hasNextPage}
-              onClick={() => handlePageChange(page + 1)}
-              className="mx-1"
-              size="sm"
-            >
-              Next
-            </CButton>
-          </CCol>
-        </CRow>
       </CCardBody>
     </CCard>
   );
