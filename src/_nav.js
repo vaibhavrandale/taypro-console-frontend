302f4041b66bd5df7202171ec125234403b9bf91
Sneha Araseed <sneha.a@taypro.in>
2025-06-11 14:08:22 +0530

    added customer feedback page for all the admin login

--- src/_nav.js
+++ src/_nav.js
@@ -204,6 +204,11 @@ const _nav = [
             name: "Technician Attendance",
             to: "/master-admin/technician-attendance",
           },
+          {
+            component: CNavItem,
+            name: "Client Feedback",
+            to: "/master-admin/client-feedback",
+          },
         ],
       },
       {
@@ -422,6 +427,11 @@ const _nav = [
             name: "Technician Attendance",
             to: "/project-admin/technician-attendance",
           },
+          {
+            component: CNavItem,
+            name: "Client Feedback",
+            to: "/project-admin/client-feedback",
+          },
         ],
       },
     ],
@@ -615,9 +625,8 @@ const _nav = [
 
       // === Attendance Group ===
       {
-        component: CNavItem,
-        name: "Technician Attendance",
-        to: "/service-admin/technician-attendance",
+        component: CNavGroup,
+        name: "Users Management",
         icon: (
           <CIcon
             icon={cilTask}
@@ -625,6 +634,18 @@ const _nav = [
             style={{ height: "30px" }}
           />
         ),
+        items: [
+          {
+            component: CNavItem,
+            name: "Technician Attendance",
+            to: "/service-admin/technician-attendance",
+          },
+          {
+            component: CNavItem,
+            name: "Client Feedback",
+            to: "/service-admin/client-feedback",
+          },
+        ],
       },
     ],
   },
