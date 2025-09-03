2ed1f56e772be6c8f637fb05373bfc778c0a7930
vaibhavrandale <127591217+vaibhavrandale@users.noreply.github.com>
2025-09-03 16:51:28 +0530

    socket url /api removed

--- src/components/Socket.js
+++ src/components/Socket.js
@@ -1,3 +1,24 @@
+// // src/socket.js
+// import { io } from "socket.io-client";
+
+// // 🔧 Use the correct backend URL
+// const backendURL =
+//   window.location.hostname === "localhost"
+//     ? "http://localhost:5000" // local dev
+//     : "https://console.taypro.in/api"; // production (force HTTPS)
+
+// const socket = io(backendURL, {
+//   path: "/socket.io", // must match Nginx/backend
+//   transports: ["websocket"],
+//   upgrade: false,
+//   autoConnect: true,
+//   secure: true, // ensure WSS
+// });
+
+// console.log("🔌 Connecting to:", backendURL);
+
+// export default socket;
+
 // src/socket.js
 import { io } from "socket.io-client";
 
@@ -5,7 +26,9 @@ import { io } from "socket.io-client";
 const backendURL =
   window.location.hostname === "localhost"
     ? "http://localhost:5000" // local dev
-    : "https://console.taypro.in/api"; // production (force HTTPS)
+    : "https://console.taypro.in"; // production (force HTTPS)
+
+// const backendURL = "https://console.taypro.in"; // production (force HTTPS)
 
 const socket = io(backendURL, {
   path: "/socket.io", // must match Nginx/backend
