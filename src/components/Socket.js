51455d67f9d47fc90ab3598a613fed10b8f48815
vaibhavrandale <127591217+vaibhavrandale@users.noreply.github.com>
2026-01-19 15:34:13 +0530

    fix: Revert socket connection configuration to use autoConnect and allow upgrade

--- src/components/Socket.js
+++ src/components/Socket.js
@@ -6,25 +6,28 @@ const backendURL =
     ? "http://localhost:5500" // local dev
     : "https://console.taypro.in"; // production (force HTTPS)
 
-// const socket = io(backendURL, {
-//   path: "/socket.io", // must match Nginx/backend
-//   // transports: ["websocket"],
-//   transports: ["polling", "websocket"], // allow upgrade
-
-//   upgrade: false,
-//   autoConnect: true,
-//   secure: true, // ensure WSS
-// });
-
 const socket = io(backendURL, {
-  path: "/socket.io",
-  transports: ["websocket"], // ✅ NO polling
-  autoConnect: false, // ✅ manual lifecycle
-  secure: true,
-  reconnection: true,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 2000,
+  path: "/socket.io", // must match Nginx/backend
+  // transports: ["websocket"],
+  transports: [
+    // "polling",
+    "websocket",
+  ], // allow upgrade
+
+  upgrade: false,
+  autoConnect: true,
+  secure: true, // ensure WSS
 });
+
+// const socket = io(backendURL, {
+//   path: "/socket.io",
+//   transports: ["websocket"], // ✅ NO polling
+//   autoConnect: false, // ✅ manual lifecycle
+//   secure: true,
+//   reconnection: true,
+//   reconnectionAttempts: 5,
+//   reconnectionDelay: 2000,
+// });
 
 console.log("🔌 Connecting to:", backendURL);
 
